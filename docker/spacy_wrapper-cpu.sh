02c734f600ceaa7d5b4c572b717034d3cb8ef3d9
James W. McClain <jmcclain@daystrom-data-concepts.com>
2022-10-26 18:13:04 -0400

    Add data sets

--- docker/spacy_wrapper-cpu.sh
+++ docker/spacy_wrapper-cpu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python -m spacy train ./config.cfg --output ${MODEL_DIR} --paths.train ./train.spacy --paths.dev ./dev.spacy
-aws s3 sync ${MODEL_DIR} s3://raster-vision-mcclain/spacy_test/$(date +"%Y%m%dT%H%M%S")/
+python -m spacy train $1 --output ${MODEL_DIR} --paths.train $2 --paths.dev $3
+# aws s3 sync ${MODEL_DIR} s3://raster-vision-mcclain/spacy_test/$(date +"%Y%m%dT%H%M%S")/
