fb5ff3b4b5f741e3fd574b17a2dc3bd135d05fdd
ravenouse <zhwa3087@colorado.edu>
2022-11-06 21:40:25 -0700

    edited the spacy-wrapper-gpu

--- docker/spacy-wrapper-gpu.sh
+++ docker/spacy-wrapper-gpu.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Starting spacy-wrapper-gpu.sh"
-curl $5 > /train/file.zip
-unzip /train/file.zip -d /train
-
 python -m spacy train $1 --output /train/output --paths.train $2 --paths.dev $3 --gpu-id 0
 aws s3 sync /train/output s3://raster-vision-mcclain/spacy_train/$4/$(date +"%Y%m%dT%H%M%S")/
