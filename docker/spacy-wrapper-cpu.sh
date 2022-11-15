89c093d1981901908ac6a811c0bf72412ed6b9e9
ravenouse <zhwa3087@colorado.edu>
2022-11-14 22:26:47 -0700

    edited the wrapper files so the wandb logs will also be transfered to s3

--- docker/spacy-wrapper-cpu.sh
+++ docker/spacy-wrapper-cpu.sh
@@ -4,4 +4,5 @@ echo "Input from: $1"
 echo "Output to: $2"
 aws s3 sync $1 /train-on-batch
 python -m spacy train /train-on-batch/config.cfg --output /train-on-batch/output --paths.train /train-on-batch/train.spacy --paths.dev /train-on-batch/dev.spacy
+aws s3 sync /train-on-batch/wandb $2/wandb
 aws s3 sync /train-on-batch/output $2/$(date +"%Y%m%dT%H%M%S")/
