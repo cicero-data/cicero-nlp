24e7026397052b7958776d9e053216e02f84fdbf
James McClain <james.mcclain@gmail.com>
2022-11-22 10:14:29 -0500

    Fairly simple batch setup (#6)
    Co-authored-by: ravenouse <zhwa3087@colorado.edu>

--- docker/spacy-wrapper-cpu.sh
+++ docker/spacy-wrapper-cpu.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-python -m spacy train $1 --output /train/output --paths.train $2 --paths.dev $3
-aws s3 sync /train/output s3://raster-vision-mcclain/spacy_test/address/$(date +"%Y%m%dT%H%M%S")/
+echo "Input from: $1"
+echo "Output to: $2"
+aws s3 sync $1 /train-on-batch
+python -m spacy train /train-on-batch/config.cfg --output /train-on-batch/output --paths.train /train-on-batch/train.spacy --paths.dev /train-on-batch/dev.spacy
+aws s3 sync /train-on-batch/wandb $2/wandb
+aws s3 sync /train-on-batch/output $2/$(date +"%Y%m%dT%H%M%S")/
